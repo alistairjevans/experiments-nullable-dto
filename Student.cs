89053829505b5c6aec46fff6f5b7db0eabd21060
Alistair Evans <alistairjevans@gmail.com>
2020-04-27 10:30:41 +0100

    Add some comments and an optional example.

--- Student.cs
+++ Student.cs
@@ -4,10 +4,15 @@ namespace NullDto
 {
     class Student 
     {
+        // warning CS8618: Non-nullable property 'FirstName' is uninitialized. Consider declaring the property as nullable.
         [Required]
         public string FirstName { get; set; }
 
+        // warning CS8618: Non-nullable property 'LastName' is uninitialized. Consider declaring the property as nullable.
         [Required]
         public string LastName { get; set; }
+
+        // No warning here, this field is actually optional (note no [Required] field).
+        public string? OptionalField { get; set; }
     }
 }
