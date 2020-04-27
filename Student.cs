7baaece1315be7f329d6f161e93bd53357bde6f3
Alistair Evans <alistairjevans@gmail.com>
2020-04-27 10:25:45 +0100

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NullDto
+{
+    class Student 
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
